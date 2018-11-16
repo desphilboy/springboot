cc5c71aee8ce3fd315a7de82d6a23772556a7be7
Iman Dezfuly <Iman.Dezfuly@anz.com>
2018-11-16 15:41:42 +1100

    initila commit

---
+++ src/main/java/iman/springboot/App.java
@@ -0,0 +1,12 @@
+package iman.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
